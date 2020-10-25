2c76b9c7ea452b7f33e3086a735be13bb0a2b91a
famg <3d.famg@gmail.com>
2020-10-25 20:32:07 -0300

    curso completo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,20 +8,51 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     private float _gravity = 9.81f;
-
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponAudio;
+    [SerializeField]
+    private int currentAmmo;
+    private int maxAmmo = 50;
+    private bool _isReloading = false;
+    private UIManager _uiManager;
+    public bool hasCoin = false;
+    [SerializeField]
+    private GameObject _weapon;
     void Start()
     {
         _controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        currentAmmo = maxAmmo;
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
     }
 
     void Update()
     {
+        if (Input.GetMouseButton(0) && currentAmmo > 0)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _weaponAudio.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.R) && _isReloading == false)
+        {
+            _isReloading = true;
+            StartCoroutine(Reload());
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+
         }
         CalculateMovement();
     }
@@ -35,5 +66,42 @@ public class Player : MonoBehaviour
 
         velocity = transform.transform.TransformDirection(velocity);
         _controller.Move(velocity * Time.deltaTime);
+    }
+    void Shoot()
+    {
+        _muzzleFlash.SetActive(true);
+        currentAmmo--;
+        _uiManager.UpdateAmmo(currentAmmo);
+        if(_weaponAudio.isPlaying == false)
+        {
+            _weaponAudio.Play();
+        }
+
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayOrigin, out hitInfo))
+        {
+            Debug.Log("Hit: " + hitInfo.transform.name);
+            GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+            Destroy(hitMarker, 0.5f);
+
+            Destructable crate = hitInfo.transform.GetComponent<Destructable>();
+            if (crate != null)
+            {
+                crate.DestroyCrate();
+            }
+
+        }
+    }
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        currentAmmo = maxAmmo;
+        _uiManager.UpdateAmmo(currentAmmo);
+        _isReloading = false;
+    }
+    public void EnableWeapon()
+    {
+        _weapon.SetActive(true);
     }
 }
